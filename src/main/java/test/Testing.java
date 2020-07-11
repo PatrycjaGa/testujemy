7b790d9cf4ab2d07e5b623d86231d23aedd89d35
Uzytkownik <pgacek03@gmail.com>
2020-07-11 14:08:31 +0200

    second commit

--- src/main/java/test/Testing.java
+++ src/main/java/test/Testing.java
@@ -2,16 +2,30 @@ package test;
 
 public class Testing {
 
-    public static void helloUsers(String[] users){
-        for (String name: users) {
+    public static void helloUsers(String[] users) {
+        for (String name : users) {
             System.out.println("Hello " + name);
         }
     }
 
+    public static String getShoppingList(String[] shoppingList) {
+        String list = "";
+        for (String names : shoppingList) {
+            list += names;
+            list += ", ";
+
+        }
+        return list;
+    }
+
+
     public static void main(String[] args) {
         String[] table = {"Michał", "Jan", "Anna"};
         helloUsers(table);
-
-
+        String[] lists = {"masło", "marchew"};
+        String shoppingList = getShoppingList(lists);
+        System.out.println(shoppingList);
     }
+
+
 }
