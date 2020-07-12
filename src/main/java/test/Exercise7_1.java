4698bb604185a0e8862e4ba6d925ad8eb2320833
Uzytkownik <pgacek03@gmail.com>
2020-07-12 12:23:40 +0200

    exercise 7

--- src/main/java/test/Exercise7_1.java
+++ src/main/java/test/Exercise7_1.java
@@ -1,4 +1,18 @@
 package test;
 
 public class Exercise7_1 {
+    public static int[] arraySeries(int lenght, int firstNumbers, int gap){
+        int[] array = new int[lenght];
+        array[0] =firstNumbers;
+        for (int i =1; i < lenght; i++){
+            array[i] = array[i-1] + gap;
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        int[] result = arraySeries(4,1,2);
+        Exercise6_1.tablePrinter(result);
+    }
+
 }
